2032c303198d5997c33b0b42132911edb9be20a5
bskiff <benskiff@msn.com>
2019-02-27 23:58:39 -0600

    Add solution to basic problems

--- StringCalc.Tests/StringCalcTest.cs
+++ StringCalc.Tests/StringCalcTest.cs
@@ -1,5 +1,7 @@
 using NUnit.Framework;
 using Interviews.StringCalculator;
+using System;
+using System.Linq;
 
 namespace Interviews.UnitTests.StringCalculator
 {
@@ -13,9 +15,62 @@ namespace Interviews.UnitTests.StringCalculator
             _stringCalc = new StringCalc();
         }
 
+        [TestCase(0, "0")]
+        [TestCase(3, "3")]
+        public void ShouldReturnSingleNumber(int expected, string given)
+        {
+            var observed = _stringCalc.add(given);
+            Assert.AreEqual(expected, observed);
+        }
+
+        [TestCase(4, "1,3")]
+        [TestCase(15, "0,15")]
+        public void ShouldAddTwoNumbers(int expected, string given) {
+            var observed = _stringCalc.add(given);
+            Assert.AreEqual(expected, observed);
+        }
+
         [Test]
-        public void ShouldFail() {
-            Assert.Fail();
+        public void ShouldAddNNumbers() {
+            Random random = new Random();
+
+            var expected = random.Next(3, 10);
+
+            var given = (new int[expected-1])
+                .Aggregate("1", (acc, _) => acc + ",1");
+
+            var observed = _stringCalc.add(given);
+            Assert.AreEqual(expected, observed);
+        }
+
+        [TestCase(4, "1\n3")]
+        [TestCase(9, "1\n3,5")]
+        public void ShouldAllowNewlineDelimeter(int expected, string given)
+        {
+            var observed = _stringCalc.add(given);
+            Assert.AreEqual(expected, observed);
+        }
+
+        [TestCase(4, "//;\n1;3")]
+        [TestCase(4, "//|\n1|3")]
+        public void ShouldSupportArbitraryDelimeters(int expected, string given)
+        {
+            var observed = _stringCalc.add(given);
+            Assert.AreEqual(expected, observed);
+        }
+
+        [TestCase("1;3")]
+        [TestCase("1|3")]
+        public void ShouldNotSupportAllDelimetersWhenNotSpecified(string given)
+        {
+            Assert.Throws<FormatException>(() => _stringCalc.add(given));
+        }
+
+        [TestCase("-1")]
+        [TestCase("1,-3")]
+        public void shouldThrowWhenGivenNegativeNumbers(string given)
+        {
+            Assert.Throws<ArgumentException>(() => _stringCalc.add(given));
         }
     }
 }
--- StringCalc/StringCalc.cs
+++ StringCalc/StringCalc.cs
@@ -1,11 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Interviews.StringCalculator
 {
     public class StringCalc
     {
         public int add(string input) {
-            return 0;
+            List<char> delimitingCharacters = new List<char>();
+            delimitingCharacters.Add(',');
+            delimitingCharacters.Add('\n');
+            if (input.StartsWith("//", StringComparison.Ordinal)) {
+                delimitingCharacters.Add(input[2]);
+                input = input.Substring(4);
+            }
+            var split = input.Split(delimitingCharacters.ToArray());
+
+            if(split.Any(s => s.Contains("-"))) {
+                throw new ArgumentException();
+            }
+
+            return split
+                .Select(Int32.Parse)
+                .Sum();
         }
     }
 }
